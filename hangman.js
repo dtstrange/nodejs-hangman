c4ef3a991abc3a7bd9b9b1a0ff4d49ab83a8e093
Dustin Frank <dustintstrange@gmail.com>
2017-12-03 16:14:37 -0800

    added inquirer question

--- hangman.js
+++ hangman.js
@@ -1,2 +1,11 @@
 //required packages
 var inquirer = require("inquirer");
+
+
+var questions = [
+    {
+        type: 'input',
+        name: 'hang_question',
+        message: "Guess a letter"
+    }
+];
