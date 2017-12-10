af8fa70f7a2944baab3e618df78bbbff775f3993
Dustin Frank <dustintstrange@gmail.com>
2017-12-10 13:23:24 -0800

    fully working

--- hangman.js
+++ hangman.js
@@ -2,22 +2,141 @@
 var inquirer = require("inquirer");
 var isLetter = require('is-letter');
 
-var count = 10;
+var Word = require("./word.js")
+//array of words to be used
+var creatures = ["BASILISK", "CENTAUR",
+    "CHIMERA", "DRAGON",
+    "GORGON", "GRIFFIN",
+    "HYDRA", "KRAKEN",
+    "MANTICORE", "MINOTAUR",
+    "PEGASUS", "PHOENIX",
+    "PSYCLOPS", "SAYTR"
+];
+require('events').EventEmitter.prototype._maxListeners = 100;
 
-var questions = function() {
+var hangman = {
+    wordBank: creatures,
+    guessesRemaining: 10,
+    guessedLetters: [],
+    currentWord: null,
 
-    console.log();
+    startGame: function () {
+        var that = this;
 
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'hang_question',
-            message: "Guess a letter"
+        if (this.guessedLetters.length > 0) {
+            this.guessedLetters = [];
         }
-    ])
+        
+        inquirer.prompt([{
+            type: "confirm",
+            name: "play",
+            message: "Want to play a game?"
+        }]).then(function(answer){
+            if(answer.play){
+                that.newGame();
+            }else{
+                console.log("Coward!")
+            }
+        })
 
-    .then(function (answers) {
+    },
 
-    })
+    newGame: function(){
+        if(this.guessesRemaining === 10){
+            console.log("Excellent, lets begin.");
+            console.log('===============');
 
-};
+            var randNum = Math.floor(Math.random()*this.wordBank.length);
+            this.currentWord = new Word(this.wordBank[randNum]);
+            this.currentWord.getLetters();
+
+            console.log(this.currentWord.wordRender());
+            this.keepPromptingUser();
+        }else{
+            this.resetGuessesRemaining();
+            this.newGame();
+        }
+    },
+
+    resetGuessesRemaining: function() {
+        this.guessesRemaining = 10;
+      },
+
+      keepPromptingUser : function(){
+        var that = this;
+        //asks player for a letter
+        inquirer.prompt([{
+            type: "input",
+            name: "chosenLtr",
+          message: "Choose a letter:",
+          validate: function(value) {
+            if(isLetter(value)){
+              return true;
+            } else{
+              return false;
+            }
+          }
+        }]).then(function(ltr) {
+          //toUpperCase because words in word bank are all caps
+          var letterReturned = (ltr.chosenLtr).toUpperCase();
+          //adds to the guessedLetters array if it isn't already there
+          var guessedAlready = false;
+            for(var i = 0; i<that.guessedLetters.length; i++){
+              if(letterReturned === that.guessedLetters[i]){
+                guessedAlready = true;
+              }
+            }
+            //if the letter wasn't guessed already run through entire function, else reprompt user
+            if(guessedAlready === false){
+              that.guessedLetters.push(ltr.letterReturned);
+    
+              var found = that.currentWord.checkIfLetterFound(letterReturned);
+              //if none were found tell user they were wrong
+              if(found === 0){
+                console.log('Nope! You guessed wrong.');
+                that.guessesRemaining--;
+                console.log('Guesses remaining: ' + that.guessesRemaining);
+                
+    
+                console.log('\n*******************');
+                console.log(that.currentWord.wordRender());
+                console.log('\n*******************');
+    
+                console.log("Letters guessed: " + that.guessedLetters);
+              } else{
+                console.log('Yes! You guessed right!');
+                  //checks to see if user won
+                  if(that.currentWord.findTheWord() === true){
+                    console.log(that.currentWord.wordRender());
+                    console.log('Congratulations! You won the game!!!');
+                    // that.startGame();
+                  } else{
+                    // display the user how many guesses remaining
+                    console.log('Guesses remaining: ' + that.guessesRemaining);
+                    console.log(that.currentWord.wordRender());
+                    console.log('\n*******************');
+                    console.log("Letters guessed: " + that.guessedLetters);
+                  }
+              }
+              if(that.guessesRemaining > 0 && that.currentWord.wordFound === false) {
+                that.keepPromptingUser();
+              }else if(that.guessesRemaining === 0){
+                console.log('Game over!');
+                console.log('The word you were guessing was: ' + that.currentWord.word);
+              }
+            } else{
+                console.log("You've guessed that letter already. Try again.")
+                that.keepPromptingUser();
+              }
+        }).catch(function (error) {
+            console.log(error);
+          });
+
+      }
+    
+
+
+
+}
+
+hangman.startGame();
--- word.js
+++ word.js
@@ -1,15 +1,7 @@
 //require letter constructor
 var Letter = require("./letter.js");
 
-//array of words to be used
-var creatures = ["BASILISK", "CENTAUR",
-"CHIMERA", "DRAGON",
-"GORGON", "GRIFFIN",
-"HYDRA", "KRAKEN",
-"MANTICORE", "MINOTAUR",
-"PEGASUS", "PHOENIX",
-"PSYCLOPS", "SAYTR"
-]
+
 
 function Word(wrd){
     var that = this;
@@ -22,13 +14,13 @@ function Word(wrd){
     this.getLetters = function(){
         for(var i=0; i<this.word.length; i++){
             var newLetter = new Letter(that.word[i]);
-            this.letters.push(newLetter);
+            this.letter.push(newLetter);
         }
     };
 
     //found the current word
     this.findTheWord = function(){
-        if(this.letters.every(function(lttr){
+        if(this.letter.every(function(lttr){
             return lttr.appear === true;
         })){
             this.wordFound = true;
@@ -39,7 +31,7 @@ function Word(wrd){
     this.checkIfLetterFound = function(guessedLetter){
         var whatToReturn = 0;
 
-        this.letters.forEach(function(lttr){
+        this.letter.forEach(function(lttr){
             if(lttr.letter === guessedLetter){
                 lttr.appear = true;
                 whatToReturn++;
@@ -51,7 +43,7 @@ function Word(wrd){
     this.wordRender = function(){
         var display = '';
 
-        this.letters.forEach(function(lttr){
+        this.letter.forEach(function(lttr){
             var currentLetter = lttr.letterRender();
             display+= currentLetter;
         });
@@ -62,4 +54,4 @@ function Word(wrd){
 };
 
 module.exports = Word;
-module.exports = creatures;
+
